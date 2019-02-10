cef1f36813eb17ce463a4d06f0f59661e20c17cc
Sznv91 <47347643+Sznv91@users.noreply.github.com>
2019-02-10 19:40:30 +0300

    Пересоздал Main

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,22 @@
 import java.util.ArrayList;
 
-public class TestClass {
+public class Main {
     public static void main (String[ ] args){
         GenerteBattleGround gbg = new GenerteBattleGround();
         gbg.generateeBattleGround(3);
         ArrayList<String[]> bg = gbg.getBattleGround();
 
         /*for (String[] one : bg){ //For testing
-            for (String two : one){
-                System.out.print(two + " ");
-            }
-            System.out.println();
+          for (String two : one){
+               System.out.print(two + " ");
+           }
+           System.out.println();
         }*/
         Fight fight = new Fight();
         fight.startFight(bg);
 
-        //TestForMethods tst = new TestForMethods();
-        //tst.startTest();
-    }
+            //TestForMethods tst = new TestForMethods();
+            //tst.startTest();
+        }
+
 }
